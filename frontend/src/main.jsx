ad0857928aeddef8c624e938c036654c503c1085
Dadabrands <Dadajoshua055@gmail.com>
2023-09-25 09:03:38 +0100

    Section 4: Starting MongoDB and Mongoose

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
