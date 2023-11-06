79cc61873972922de896cf4838c9af74cd442403
Dadabrands <Dadajoshua055@gmail.com>
2023-11-06 11:56:49 +0100

    build

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 
   // any route that is not api will be redirected to index.html
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   const __dirname = path.resolve();
